02e6f0037ec664446c999a4e1d5a9e3ce3065f1c
vineetkashyap <vkvineet66@gmail.com>
2021-03-14 19:53:29 +0530

    change database and and url

--- Myjob/settings.py
+++ Myjob/settings.py
@@ -89,6 +89,9 @@ DATABASES = {
          'PORT': '5432',
     }
 }
+import dj_database_url
+db_from_env= dj_database_url.config(conn_max_age=600)
+DATABASE['default'].update(db_from_env)
 
 
 # Password validation
