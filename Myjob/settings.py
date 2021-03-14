89286f35bdceb2a86adc70f06e3d464730510eff
vineetkashyap <vkvineet66@gmail.com>
2021-03-14 21:19:26 +0530

    change static location  in setting

--- Myjob/settings.py
+++ Myjob/settings.py
@@ -166,6 +166,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-DEBUG_PROPAGATE_EXCEPTIONS = True
+# DEBUG_PROPAGATE_EXCEPTIONS = True
 COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorag'
